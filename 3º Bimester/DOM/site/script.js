6c0d965d613f08706217482dd895b40cf63f3c3b
Gabriel Tavares <gabriel.tavares.1904@gmail.com>
2023-07-04 17:53:07 -0300

    Upload: Dom Activity

---
+++ 3º Bimester/DOM/site/script.js
@@ -0,0 +1,14 @@
+document.getElementById("S").style.backgroundColor = "red";
+document.getElementById("S").style.color = "black";
+
+document.getElementsByName("A")[0].style.backgroundColor = "orange";
+document.getElementsByName("A")[0].style.color = "black";
+
+document.getElementsByTagName("th")[3].style.backgroundColor = "yellow";
+document.getElementsByTagName("th")[3].style.color = "black";
+
+document.getElementsByClassName("green")[0].style.backgroundColor = "green";
+document.getElementsByClassName("green")[0].style.color = "black";
+
+document.querySelector("th#D").style.backgroundColor = "cyan";
+document.querySelector("th#D").style.color = "black";
